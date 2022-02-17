d954c30bc2c92178afe882719df743e82ce07f09
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2022-02-17 01:49:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let element = document.getElementById('datetime').innerHTML
+var span = document.getElementById('span');
+
 var currentdate = new Date();
 var datetime = currentdate.getDate() + "/"
     + (currentdate.getMonth()+1)  + "/"
@@ -8,7 +9,6 @@ var datetime = currentdate.getDate() + "/"
     + currentdate.getSeconds();
 //changes according to computer timezone, love it
 
-
-element.textContent = datetime;
+span.textContent = datetime
 
 setInterval(time, 1000);
