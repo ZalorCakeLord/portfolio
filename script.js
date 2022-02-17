1a660236dfbcead71b98347d85c7a2c76f70e362
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2022-02-17 01:59:58 -0600

    live clock

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+function time() {
+  var currentdate = new Date();
+  var datetime = currentdate.getDate() + "/"
+      + (currentdate.getMonth()+1)  + "/"
+      + currentdate.getFullYear() + " @ "
+      + currentdate.getHours() + ":"
+      + currentdate.getMinutes() + ":"
+      + currentdate.getSeconds();
+  document.getElementById('datetime').innerHTML=datetime;}
 
-var currentdate = new Date();
-var datetime = currentdate.getDate() + "/"
-    + (currentdate.getMonth()+1)  + "/"
-    + currentdate.getFullYear() + " @ "
-    + currentdate.getHours() + ":"
-    + currentdate.getMinutes() + ":"
-    + currentdate.getSeconds();
-//changes according to computer timezone, love it
-
-document.getElementById('datetime').innerHTML=datetime;
+setInterval(time, 1000);
